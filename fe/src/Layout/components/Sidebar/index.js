53ab3b9a0cbf1521ed139ecc43a45183dd2f4558
TruongNg <truongng1312@gmail.com>
2025-07-16 00:39:53 +0700

    change title -> img

--- fe/src/Layout/components/Sidebar/index.js
+++ fe/src/Layout/components/Sidebar/index.js
@@ -9,13 +9,15 @@ import {
     faHouse,
     faUser,
 } from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
 
 function Sidebar() {
     return (
         <div className="sidebar position-fixed h-100">
             {/* change m -> p */}
             <div className="d-flex flex-column align-items-center h-100 p-4">
-                <h1 className="m-0 pt-3 pb-5 text-white fw-bold">Admin</h1>
+                <img className="mt-4 mb-5 object-fit-contain rounded-circle" src={images.faruzanCat} alt="faru-img" />
+                {/* <h1 className="m-0 pt-3 pb-5 text-white fw-bold">Admin</h1> */}
                 <SidebarItem to={config.routes.adminHome} title="Trang chủ" icon={faHouse} />
                 <SidebarItem to={config.routes.adminProducts} title="Sản phẩm" icon={faBoxOpen} />
                 <SidebarItem to={config.routes.adminUsers} title="Người dùng" icon={faUser} />
